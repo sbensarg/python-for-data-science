3c5d4170631dc8d333bb7b02e6dbcb94a1a698a4
Sara Bensarghin <sbensarg@e1r11p5.1337.ma>
2024-12-07 22:51:30 +0100

    add some changes

--- starting/ex09/setup.py
+++ starting/ex09/setup.py
@@ -1,3 +1,18 @@
-from setuptools import setup
+import setuptools
 
-setup()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ft_package",
+    version="0.0.1",
+    author="sbensarg",
+    author_email="sbensarg@student.1337.ma",
+    description="A sample test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sbensarg",
+    license="MIT",
+    python_requires=">=3.10"
+)
--- starting/ex09/test_ft_package.py
+++
@@ -1,5 +0,0 @@
-from ft_package import count_in_list
-
-# Test cases
-print(count_in_list(["toto", "tata", "toto"], "toto"))  # Expected output: 2
-print(count_in_list(["toto", "tata", "toto"], "tutu"))  # Expected output: 0
